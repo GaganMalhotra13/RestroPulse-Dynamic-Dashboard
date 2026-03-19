cc01ce54a50537de10d6920fc5b2dbb77ee25bf2
GaganMalhotra13 <malhotragagan1996@gmail.com>
2026-03-20 02:26:20 +0530

    Check: env variable logging

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,9 +3,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Backend Api
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl:
-      process.env.REACT_APP_BASE_URL ||
-      "http://localhost:5001",
+    baseUrl: "https://restropulse-backend.onrender.com",
+    // baseUrl:
+    //   process.env.REACT_APP_BASE_URL ||
+    //   "http://localhost:5001",
   }), // base url
   reducerPath: "adminApi",
   // tags
